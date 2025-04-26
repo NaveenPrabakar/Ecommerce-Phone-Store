a8ea049e84e64ae84fe3c77d977f13400f0a1e84
Naveen Prabakar <nvnprabakar@gmail.com>
2025-04-26 10:06:34 -0500

    tailwind

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -5,13 +5,16 @@ const Login = ({setStep}) => {
 
   return (
     //bootstrap template for sign up
-    <div style={{ backgroundColor: "#800000" }}>
-      <section className="vh-100" style={{ backgroundColor: "#eee" }}>
+    <div>
+      <section className="vh-100">
         <div className="container h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col-lg-12 col-xl-11">
-              <div className="card text-black" style={{ borderRadius: "25px" }}>
+              <div className="bg-black card text-white" style={{ borderRadius: "25px" }}>
                 <div className="card-body p-md-5">
+                <h1 className="text-center">
+                    Phones Glore!
+                  </h1>
                   <div className="row justify-content-center">
                     <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                       <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
@@ -56,7 +59,7 @@ const Login = ({setStep}) => {
                         <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                           <button
                             type="button"
-                            className="btn btn-primary btn-lg"
+                            className="btn btn-primary btn-lg hover:bg-red-500"
                           >
                             Login
                           </button>
@@ -65,7 +68,7 @@ const Login = ({setStep}) => {
                         <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                           <button
                             type="button"
-                            className="btn btn-primary btn-lg"
+                            className="btn btn-primary btn-lg hover:bg-red-500"
                             onClick ={() => setStep("signup")}
                           >
                             Don't have an account? SignUp!
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -5,13 +5,16 @@ import Login from "./Login";
 const SignUpForm = ({setStep}) => {
   return (
     //bootstrap template for sign up
-    <div style={{ backgroundColor: "#800000" }}>
-      <section className="vh-100" style={{ backgroundColor: "#eee" }}>
+    <div>
+      <section className="vh-100">
         <div className="container h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col-lg-12 col-xl-11">
-              <div className="card text-black" style={{ borderRadius: "25px" }}>
+              <div className="bg-black card text-white" style={{ borderRadius: "25px" }}>
                 <div className="card-body p-md-5">
+                <h1 className="text-center">
+                    Phones Glore!
+                  </h1>
                   <div className="row justify-content-center">
                     <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                       <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
