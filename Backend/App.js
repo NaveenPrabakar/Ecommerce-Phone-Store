87dc12d04032046882af9705196d8875542316f8
Naveen Prabakar <nvnprabakar@gmail.com>
2025-04-26 13:04:33 -0500

    added sign up backend, need to conect front and back now

--- Backend/App.js
+++ Backend/App.js
@@ -15,7 +15,7 @@ const host = "localhost";
 
 //connect to mongoDB
 const url = "mongodb://127.0.0.1:27017";
-const dbName = "secoms319";
+const dbName = "phones";
 const { MongoClient } = require("mongodb");
 const client = new MongoClient(url);
 const db = client.db(dbName);
@@ -23,5 +23,35 @@ const db = client.db(dbName);
 
 app.listen(port, () => {
     console.log("App listening at http://%s:%s", host, port);
+});
+
+
+
+//Post request to sign up
+app.post("/signup", async (req, res) =>{
+    await client.connect();
+    console.log("Node connected successfully to POST MongoDB");
+
+    const user = req.body;
+
+    try{
+        const newuser = await db.collection("phones").insertOne(user);
+        console.log("User signed up: ", newuser);
+        res.status(200);
+        res.send("User signed up!");
+
+    }
+    catch(e){
+        res.status(500);
+        res.send("Error sending to MongoDB");
+    }
+
+    res.status(200);
+});
+
+
+//Get request to sign up
+app.get("/login", async(req, res) => {
+    
 });
 
