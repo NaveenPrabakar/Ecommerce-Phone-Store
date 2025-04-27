b82ef9be06e30f0f0826cd242c2ca5301e3ce415
Naveen Prabakar <nvnprabakar@gmail.com>
2025-04-26 23:21:14 -0500

    Set up the start off the home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,15 +5,18 @@ import "./App.css";
 
 import SignUpForm from "./components/Signup";
 import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
   const [step, setStep] = useState("login"); //switch between the pages
   const [success, setSuccess] = useState(""); //display success creation of account only
+  const [user, setUser] = useState(null); //which user logged on
 
   return (
     <div className = "app-container">
-      {step == "login" && <Login setStep={setStep} setSuccess={setSuccess} success={success} />}
+      {step == "login" && <Login setStep={setStep} setSuccess={setSuccess} success={success} setUser={setUser} />}
       {step == "signup" && <SignUpForm setStep={setStep} setSuccess={setSuccess} />}
+      {step == "home" && <Home setUser={setUser} user={user} />}
     </div>
   );
 };
---
+++ Frontend/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState } from "react";
+import { Phones } from "../data/phone.json";
+
+const Home = ({ setUser, user }) => {
+
+};
+
+export default Home;
