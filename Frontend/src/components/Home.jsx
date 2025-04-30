0211a83b76fe097561dd262609fd1a6d5c688d0d
Naveen Prabakar <nvnprabakar@gmail.com>
2025-04-29 21:23:11 -0500

    added a nav bar for consistency

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Phones } from "../data/phone";
+import NavBar from "./NavBar"
 
 const Home = ({ setProf, prof }) => {
   const preview = []; //only display three phones on the phone screen
@@ -11,38 +12,7 @@ const Home = ({ setProf, prof }) => {
 
   return (
     <div>
-      <header className="bg-blue-500 shadow-md p-4 text-sm items-center justify-center">
-        <h2 className="text-left font-serif">Hi {prof.Name}!</h2>
-      </header>
-
-      <div className="bg-green-500 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:grid-cols-4 gap-6 p-6">
-        {preview.map((c) => (
-          <div
-            key={c.id}
-            className="bg-white rounded-lg shadow-md p-4 text-sm items-center justify-center hover:bg-black hover:text-white"
-          >
-            <img
-              src={c.thumbnail}
-              alt={c.title}
-              className="flex justify-center items-center"
-            />
-            <div className="flex justify-between mt-4">
-              <h3 className="text-sm font-semibold text-gray-700">
-                Model: {c.title}
-              </h3>
-              <h3 className="text-green-700 font-extrabold text-sm">
-                ${c.price}
-              </h3>
-            </div>
-
-            <div className="flex justify-between mt-4">
-              <h3 className="text-sm font-semibold text-black-600">
-                Brand: {c.brand}
-              </h3>
-            </div>
-          </div>
-        ))}
-      </div>
+      <NavBar />
     </div>
   );
 };
---
+++ Frontend/src/components/NavBar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Home from '../assets/home.png'
+import Shop from "../assets/shop.png"
+import Sell from "../assets/Sell.png"
+import Cart from "../assets/cart.png"
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="#">
+                    <img src={Home} width="30" height="30" className="d-inline-block align-top" alt="Home"/>
+                   Home
+                </a>
+
+                <a className="navbar-brand" href="#">
+                    <img src={Shop} width="30" height="30" className="d-inline-block align-top" alt="Home"/>
+                   Shop
+                </a>
+
+                <a className="navbar-brand" href="#">
+                    <img src={Sell} width="30" height="30" className="d-inline-block align-top" alt="Home"/>
+                   Sell
+                </a>
+
+                <a className="navbar-brand" href="#">
+                    <img src={Cart} width="30" height="30" className="d-inline-block align-top" alt="Home"/>
+                   Cart
+                </a>
+            </nav>
+        </div>
+    );
+};
+
+export default NavBar;
