46de3e6e9d992e7a1f12ea1e0b7f84a391163f11
Milo M <milomucu@iastate.edu>
2025-05-04 09:59:47 -0500

    Initial Admin View Frontend

---
+++ Frontend/.vite/deps/react-bootstrap_Form.js
@@ -0,0 +1,7 @@
+import {
+  Form_default
+} from "./chunk-3GAKQIPJ.js";
+import "./chunk-5WQJO2FO.js";
+export {
+  Form_default as default
+};
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./components/Login";
 import Home from "./components/Home";
 import Shop from "./components/Shop";
 import Purchasing from "./components/Purchasing";
+import Admin from "./components/Admin";
 import Sell from "./components/Sell";
 
 function App() {
@@ -15,13 +16,14 @@ function App() {
   const [success, setSuccess] = useState(""); //display success creation of account only
   const [prof, setProf] = useState(null); //which user logged on
   const [cart, setCart] = useState([]); //Items in cart
-  const [addItem, setItem] = useState(); //Items in cart
+  const [addItem, setItem] = useState(); //Item to be added to cart
 
   return (
     <div className = "app-container">
       {step == "login" && <Login setStep={setStep} setSuccess={setSuccess} success={success} setProf={setProf} />}
       {step == "signup" && <SignUpForm setStep={setStep} setSuccess={setSuccess} />}
       {step == "home" && <Home setStep={setStep} setProf={setProf} prof={prof} />}
+      {step == "admin" && <Admin setStep={setStep} setProf={setProf} prof={prof} setItem={setItem}/>}
       {step == "shop" && <Shop setStep={setStep} setProf={setProf} prof={prof} setItem={setItem}/>}
       {step == "purchasing" && <Purchasing setStep={setStep} setProf={setProf} prof={prof} setCart={setCart} cart={cart} addItem={addItem}/>}
       {step == "sell" && <Sell setStep={setStep} setProf={setProf} prof={prof} />}   
