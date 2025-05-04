1009f4a0b2d978d532a722e2f9284e695f7f7138
Naveen Prabakar <nvnprabakar@gmail.com>
2025-05-04 11:07:08 -0500

    Finishing up crud for login/sign up

---
+++ Frontend/src/components/Settings.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Phones } from "../data/phone";
+import NavBar from "./NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Row, Col, Container, Button } from "react-bootstrap";
+import Footer from "./Footer";
